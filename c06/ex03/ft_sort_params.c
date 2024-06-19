08a19c193a74358aa73841a11b736789147aa84b
Sarah <161157953+sarahgh8@users.noreply.github.com>
2024-06-19 15:21:54 +0300

    Add files via upload

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+
+int ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+void	printparams (int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			write (1, &argv[i][j], 1);
+			j++;
+		}
+		write (1, "\n", 1);
+		i++;
+	}
+}
+
+
+int main (int argc, char **argv)
+{
+	int	i;
+	int	j;
+	char	*temp;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i],  argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+	printparams (argc, argv);
+
+}
