19ea157571ffc051ac2d091180e295a5d2ab7d7d
Sarah <161157953+sarahgh8@users.noreply.github.com>
2024-06-19 15:22:58 +0300

    Add files via upload

---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,27 @@
+//#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	while (i < size/2)
+	{
+		temp = tab[i];
+		tab[i] = tab[size - i - 1];
+		tab[size - i - 1] = temp;
+		i++;
+	}
+}
+/*int main ()
+{
+	int arr[] = {1, 2, 3 , 4, 5};
+	ft_rev_int_tab(arr, 5);
+	for (int i = 0; i<5; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return (0);
+}*/
---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,38 @@
+//#include <stdio.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	j = i + 1;
+
+	while (i < size)
+	{
+		while (j < size)
+		{
+			if (tab[j] < tab[i])
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+}
+/*int main ()
+{
+	int arr[] = {4, 76, 23 , 90, 21};
+	ft_sort_int_tab(arr, 5);
+	for (int i = 0; i<5; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return (0);
+}*/
